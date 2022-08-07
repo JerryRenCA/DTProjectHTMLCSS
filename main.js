5bc883acac98fa1355d964ff14efd23fda6022c1
Kevin <renjinxuan>
2022-08-07 11:57:26 -0300

    Go

---
+++ main.js
@@ -0,0 +1,9 @@
+//execute script after page load
+window.onload = function digital_fn()
+{
+    let toggle= document.querySelector("#nav .toggle-btn");
+    let collapse = document.querySelector("#nav .collapse");
+    toggle.addEventListener('click',function(event){
+        collapse.classList.toggle('active');
+    });
+}
